944b115fe78994b8448319c80c57d1723877abd1
Yanick Champoux <yanick@babyl.dyndns.org>
2013-05-31 13:04:26 -0400

    Use a yaml master file
    ... from which both the Markdown and CSV are created.

---
+++ scripts/yaml2csv.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+
+use autodie;
+
+use YAML qw/ LoadFile /;
+use Text::CSV;
+
+my $csv = Text::CSV->new({ 
+    binary              => 1,  
+    auto_diag => 1,
+    allow_whitespace => 1,
+    always_quote => 1,
+    eol => "\n",
+    });
+
+my @companies = sort { $a->{name} cmp $b->{name} }
+                     @{ LoadFile( 'Perl_Companies.yaml' ) };
+
+open my $fh, '>', 'Perl_Companies.csv';
+
+$csv->print( $fh => [
+    "Company Name",
+    "Company Location",
+    "Most Recent Posting",
+    "Hiring Status",
+]);
+
+for( @companies ) {
+    $csv->print( $fh => [
+        @{$_}{qw/ name location most_recent_posting hiring_status /}
+    ]);
+}
+
+
